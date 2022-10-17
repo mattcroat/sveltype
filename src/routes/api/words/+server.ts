23b0227474c1f3284a06ce7046c6d09ad6945876
mattcroat <mattcroat.dev@gmail.com>
2022-10-17 12:46:31 +0200

    chore: Rename amount to limit

--- src/routes/api/words/+server.ts
+++ src/routes/api/words/+server.ts
@@ -4,7 +4,7 @@ import type { RequestHandler } from './$types'
 import english from './languages/english.json'
 
 export const GET: RequestHandler = ({ url }) => {
-	const amount = Number(url.searchParams.get('amount'))
-	const words = english.words.slice(0, amount)
+	const limit = Number(url.searchParams.get('limit'))
+	const words = english.words.slice(0, limit).sort(() => 0.5 - Math.random())
 	return json(words)
 }
